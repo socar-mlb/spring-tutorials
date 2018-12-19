10357ebe34f8996030e5336b368f805753e953a4
socar-mlb <mlb@socar.kr>
2018-12-19 12:31:38 +0900

    unable auto configuration

--- myproject/src/main/kotlin/org/inframincer/myproject/MyProjectApplication.kt
+++ myproject/src/main/kotlin/org/inframincer/myproject/MyProjectApplication.kt
@@ -1,12 +1,23 @@
 package org.inframincer.myproject
 
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.SpringBootConfiguration
+import org.springframework.boot.WebApplicationType
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
 
-@SpringBootApplication
+//@SpringBootApplication
+//@SpringBootConfiguration
+@Configuration
+@ComponentScan
+//@EnableAutoConfiguration
 class MyProjectApplication
-
 fun main(args: Array<String>) {
-	runApplication<MyProjectApplication>(*args)
+//	runApplication<MyProjectApplication>(*args)
+	val application = SpringApplication(MyProjectApplication::class.java)
+	application.webApplicationType = WebApplicationType.NONE
+	application.run(*args)
 }
-
