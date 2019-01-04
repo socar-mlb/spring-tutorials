7b7fb72643b0982ca1423a5c4ddee3459baf8e0d
socar-mlb <mlb@socar.kr>
2019-01-04 15:13:13 +0900

    bean scope prototype, scope proxy mode: target class

--- ioc/src/main/kotlin/org/inframincer/ioc/Proto.kt
+++ ioc/src/main/kotlin/org/inframincer/ioc/Proto.kt
@@ -1,9 +1,10 @@
 package org.inframincer.ioc
 
 import org.springframework.context.annotation.Scope
+import org.springframework.context.annotation.ScopedProxyMode
 import org.springframework.stereotype.Component
 
 @Component
-@Scope("prototype")
+@Scope(value="prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
 class Proto {
 }
