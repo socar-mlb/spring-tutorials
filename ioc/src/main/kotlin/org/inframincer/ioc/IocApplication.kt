b64cf8c24826a26a5784b2ff397b341abe48f343
socar-mlb <mlb@socar.kr>
2019-01-04 14:25:11 +0900

    component annotation and functional bean definition

--- ioc/src/main/kotlin/org/inframincer/ioc/IocApplication.kt
+++ ioc/src/main/kotlin/org/inframincer/ioc/IocApplication.kt
@@ -1,11 +1,27 @@
 package org.inframincer.ioc
 
+import org.inframincer.ioc2.MyBook3Repository
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationContextInitializer
+import org.springframework.context.support.GenericApplicationContext
+import java.util.function.Supplier
 
 @SpringBootApplication
 class IocApplication
 
 fun main(args: Array<String>) {
-    runApplication<IocApplication>(*args)
+//    runApplication<IocApplication>(*args)
+    var application = SpringApplication(IocApplication::class.java)
+    application.addInitializers(ApplicationContextInitializer<GenericApplicationContext> {
+        applicationContext -> applicationContext.registerBean(MyBook3Repository::class.java)
+        applicationContext.registerBean(ApplicationRunner::class.java, Supplier<ApplicationRunner> {
+                ApplicationRunner {
+                    println("Functional Bean Definition!!")
+                }
+            })
+    })
+    application.run(*args)
+
 }
---
+++ ioc/src/main/kotlin/org/inframincer/ioc2/MyBook3Repository.kt
@@ -0,0 +1,6 @@
+package org.inframincer.ioc2
+
+import org.inframincer.ioc.BookRepository
+
+class MyBook3Repository: BookRepository {
+}
