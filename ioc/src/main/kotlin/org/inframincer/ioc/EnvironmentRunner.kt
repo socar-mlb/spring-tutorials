284f0f85ad617ba9d2013963a4208aa4469fd761
socar-mlb <mlb@socar.kr>
2019-01-15 08:37:18 +0900

    environment property

--- ioc/src/main/kotlin/org/inframincer/ioc/EnvironmentRunner.kt
+++ ioc/src/main/kotlin/org/inframincer/ioc/EnvironmentRunner.kt
@@ -1,6 +1,7 @@
 package org.inframincer.ioc
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.context.ApplicationContext
@@ -18,6 +19,9 @@ class EnvironmentRunner: ApplicationRunner {
     @Autowired
     lateinit var testBook2Repository: TestBook2Repository
 
+    @Value("\${app.value}")
+    val appName: String = ""
+
     override fun run(args: ApplicationArguments?) {
         val environment = context.environment
 
@@ -25,5 +29,11 @@ class EnvironmentRunner: ApplicationRunner {
             println(profile)
         for (profile in environment.defaultProfiles)
             println(profile)
+
+        // Environment VM options : -Dapp.name=spring5
+        println(environment.getProperty("app.name"))
+        // Environment app.properties
+        println(environment.getProperty("app.about"))
+        println(appName)
     }
 }
--- ioc/src/main/kotlin/org/inframincer/ioc/IocApplication.kt
+++ ioc/src/main/kotlin/org/inframincer/ioc/IocApplication.kt
@@ -2,8 +2,10 @@ package org.inframincer.ioc
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.PropertySource
 
 @SpringBootApplication
+@PropertySource("classpath:/app.properties")
 class IocApplication
 
 fun main(args: Array<String>) {
