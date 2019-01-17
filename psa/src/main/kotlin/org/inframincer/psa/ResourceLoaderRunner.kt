2c7e1f5be1e164c8778dc2b1d7bea6fc50cef720
socar-mlb <mlb@socar.kr>
2019-01-17 11:59:47 +0900

    portable service abstraction: resource

--- psa/src/main/kotlin/org/inframincer/psa/ResourceLoaderRunner.kt
+++ psa/src/main/kotlin/org/inframincer/psa/ResourceLoaderRunner.kt
@@ -3,6 +3,7 @@ package org.inframincer.psa
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
+import org.springframework.context.ApplicationContext
 import org.springframework.core.io.ResourceLoader
 import org.springframework.stereotype.Component
 import java.io.File
@@ -12,6 +13,9 @@ class ResourceLoaderRunner : ApplicationRunner {
 
     @Autowired
     lateinit var resourceLoader: ResourceLoader
+
+    @Autowired
+    lateinit var applicationContext: ApplicationContext
 
     override fun run(args: ApplicationArguments?) {
         val resource = resourceLoader.getResource("classpath:test.txt")
@@ -41,5 +45,15 @@ class ResourceLoaderRunner : ApplicationRunner {
         readLines.forEach {
             println(it)
         }
+
+        // class org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext
+        println(applicationContext.javaClass)
+
+        val resource1 = resourceLoader.getResource("test.txt")
+        val resource2 = resourceLoader.getResource("classpath:test.txt")
+        val resource3 = resourceLoader.getResource("file:///test.txt")
+        println(resource1.javaClass) // class org.springframework.web.context.support.ServletContextResource
+        println(resource2.javaClass) // class org.springframework.core.io.ClassPathResource
+        println(resource3.javaClass) // class org.springframework.core.io.FileUrlResource
     }
 }
