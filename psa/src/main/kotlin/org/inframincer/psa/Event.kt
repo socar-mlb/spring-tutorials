52b70a25596b18581f71612fca4139f6f509cbff
socar-mlb <mlb@socar.kr>
2019-01-17 12:58:11 +0900

    spring psa validation using local validator factory bean

--- psa/src/main/kotlin/org/inframincer/psa/Event.kt
+++ psa/src/main/kotlin/org/inframincer/psa/Event.kt
@@ -1,6 +1,18 @@
 package org.inframincer.psa
 
+import javax.validation.constraints.Email
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotEmpty
+
 class Event {
     var id: Int = 0
+
+    @NotEmpty
     var title: String = ""
+
+    @Min(0)
+    var limit: Int = 0
+
+    @Email
+    var email: String = ""
 }
--- psa/src/main/kotlin/org/inframincer/psa/ValidatorRunner.kt
+++ psa/src/main/kotlin/org/inframincer/psa/ValidatorRunner.kt
@@ -1,12 +1,24 @@
 package org.inframincer.psa
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
+import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 import org.springframework.validation.BeanPropertyBindingResult
+import org.springframework.validation.Validator
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
 
 @Component
 class ValidatorRunner : ApplicationRunner{
+
+    @Autowired
+    lateinit var validator: Validator
+
+    @Bean
+    fun validator(): Validator {
+        return LocalValidatorFactoryBean()
+    }
 
     override fun run(args: ApplicationArguments?) {
         val event = Event()
@@ -14,6 +26,21 @@ class ValidatorRunner : ApplicationRunner{
         val errors = BeanPropertyBindingResult(event, "event")
 
         eventValidator.validate(event, errors)
+        println(errors.hasErrors())
+
+        errors.allErrors.forEach {
+            it.codes!!.forEach {
+                println(it)
+            }
+            println(it.defaultMessage)
+        }
+
+        println(validator.javaClass)
+
+        event.limit = -1
+        event.email = "aaa"
+
+        validator.validate(event, errors)
         println(errors.hasErrors())
 
         errors.allErrors.forEach {
