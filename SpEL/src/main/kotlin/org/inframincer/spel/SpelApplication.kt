4bf3d8398311fa5d71f7b6b0aa84f6b5759d5303
socar-mlb <mlb@socar.kr>
2019-01-22 13:08:10 +0900

    spring expression language initializr

---
+++ SpEL/src/main/kotlin/org/inframincer/spel/SpelApplication.kt
@@ -0,0 +1,12 @@
+package org.inframincer.spel
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpelApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpelApplication>(*args)
+}
+
