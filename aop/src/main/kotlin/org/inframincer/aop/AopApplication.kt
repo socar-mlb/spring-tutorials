9165e19efe509391543bef34ac258b77b264dde8
socar-mlb <mlb@socar.kr>
2019-01-22 16:28:57 +0900

    spring aop initializr

---
+++ aop/src/main/kotlin/org/inframincer/aop/AopApplication.kt
@@ -0,0 +1,12 @@
+package org.inframincer.aop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AopApplication
+
+fun main(args: Array<String>) {
+    runApplication<AopApplication>(*args)
+}
+
